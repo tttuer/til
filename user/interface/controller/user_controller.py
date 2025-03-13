3f60bbaa209fe6d83a815d192d3653357f0a52f6
tttuer <tttuer1@naver.com>
2025-03-13 10:55:58 +0900

    use pydantic - declare response type

--- user/interface/controller/user_controller.py
+++ user/interface/controller/user_controller.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 from dependency_injector.wiring import inject, Provide
 from fastapi import APIRouter, Depends
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, EmailStr
 from starlette import status
 
 from containers import Container
@@ -10,21 +12,36 @@ router = APIRouter(prefix="/users", tags=["users"])
 
 
 class CreateUserBody(BaseModel):
-    name: str
-    email: str
-    password: str
+    name: str = Field(min_length=2, max_length=32)
+    email: EmailStr = Field(max_length=64)
+    password: str = Field(min_length=8, max_length=32)
 
 
 class UpdateUser(BaseModel):
-    name: str | None = None
-    password: str | None = None
+    name: str | None = Field(min_length=2, max_length=32, default=None)
+    password: str | None = Field(min_length=8, max_length=32, default=None)
+
+
+class UserResponse(BaseModel):
+    id: str
+    name: str
+    email: str
+    created_at: datetime
+    updated_at: datetime
+
+
+class GetUsersResponse(BaseModel):
+    total_count: int
+    page: int
+    users: list[UserResponse]
 
 
 @router.post("", status_code=status.HTTP_201_CREATED)
 @inject
 def create_user(user: CreateUserBody,
                 user_service: UserService = Depends(
-                    Provide[Container.user_service])):
+                    Provide[Container.user_service])
+                ) -> UserResponse:
     created_user = user_service.create_user(
         name=user.name,
         email=user.email,
@@ -53,7 +70,8 @@ def update_user(user_id: str,
 def get_users(
         page: int = 1,
         items_per_page: int = 10,
-        user_service: UserService = Depends(Provide[Container.user_service])):
+        user_service: UserService = Depends(Provide[Container.user_service])
+) -> GetUsersResponse:
     total_count, users = user_service.get_users(page, items_per_page)
 
     return {
